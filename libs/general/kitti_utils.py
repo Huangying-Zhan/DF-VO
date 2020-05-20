a75219ce529b41fa98cf409d85cb23efab9a6bf8
Huangying-Zhan <zhanhuangying@gmail.com>
2020-05-20 18:32:13 +0930

    update function docstrings

--- libs/general/kitti_utils.py
+++ libs/general/kitti_utils.py
@@ -10,7 +10,14 @@ __email__ = "lee.clement@robotics.utias.utoronto.ca"
 
 
 def rotx(t):
-    """Rotation about the x-axis."""
+    """Rotation about the x-axis.
+    
+    Args:
+        t (float): angle
+    
+    Returns:
+        R (array, [3x3]): rotation matrix
+    """
     c = np.cos(t)
     s = np.sin(t)
     return np.array([[1,  0,  0],
@@ -19,7 +26,14 @@ def rotx(t):
 
 
 def roty(t):
-    """Rotation about the y-axis."""
+    """Rotation about the y-axis.
+
+    Args:
+        t (float): angle
+    
+    Returns:
+        R (array, [3x3]): rotation matrix
+    """
     c = np.cos(t)
     s = np.sin(t)
     return np.array([[c,  0,  s],
@@ -28,7 +42,15 @@ def roty(t):
 
 
 def rotz(t):
-    """Rotation about the z-axis."""
+    """Rotation about the z-axis.
+
+    Args:
+        t (float): angle
+    
+    Returns:
+        R (array, [3x3]): rotation matrix
+    
+    """
     c = np.cos(t)
     s = np.sin(t)
     return np.array([[c, -s,  0],
@@ -37,14 +59,29 @@ def rotz(t):
 
 
 def transform_from_rot_trans(R, t):
-    """Transforation matrix from rotation matrix and translation vector."""
+    """Transforation matrix from rotation matrix and translation vector.
+    
+    Args:
+        R (array, [3x3]): rotation matrix
+        t (float): angle
+    
+    Returns:
+        T (array, [4x4]): transformation matrix
+    """
     R = R.reshape(3, 3)
     t = t.reshape(3, 1)
     return np.vstack((np.hstack([R, t]), [0, 0, 0, 1]))
 
 
 def read_calib_file(filepath):
-    """Read in a calibration file and parse into a dictionary."""
+    """Read in a calibration file and parse into a dictionary.
+
+    Args:
+        filepath (str): file path
+    
+    Return:
+        data (dict): a dictionary containing intrinsics for different sensors
+    """
     data = {}
 
     with open(filepath, 'r') as f:
@@ -61,7 +98,17 @@ def read_calib_file(filepath):
 
 
 def load_stereo_pairs(imL_files, imR_files, **kwargs):
-    """Helper method to read stereo image pairs."""
+    """Helper method to read stereo image pairs.
+    
+    Args:
+        imL_files (list): left image file paths
+        imR_files (list): right image file paths
+
+    Returns:
+        a list containing tuples 
+            - **left** (HxWxC): left image
+            - **right** (HxWxC): left image
+    """
     StereoPair = namedtuple('StereoPair', 'left, right')
 
     impairs = []
@@ -87,7 +134,14 @@ def load_stereo_pairs(imL_files, imR_files, **kwargs):
 
 
 def load_velo_scans(velo_files):
-    """Helper method to parse velodyne binary files into a list of scans."""
+    """Helper method to parse velodyne binary files into a list of scans.
+    
+    Args:
+        velo_files (list): velodyne data paths
+    
+    Returns:
+        scan_list (list): list of velodyne scans, each scan is a [Nx4] array
+    """
     scan_list = []
     for filename in velo_files:
         scan = np.fromfile(filename, dtype=np.float32)

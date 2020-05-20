0c23f84cbe77101abba61463249062a34cd0262f
Huangying-Zhan <zhanhuangying@gmail.com>
2020-05-20 21:56:48 +0930

    update timer

--- libs/general/timer.py
+++ libs/general/timer.py
@@ -1,34 +1,106 @@
-# Copyright (C) Huangying Zhan 2019. All rights reserved.
-# This software is licensed under the terms in the LICENSE file 
-# which allows for non-commercial use only.
+''''''
+'''
+@Author: Huangying Zhan (huangying.zhan.work@gmail.com)
+@Date: 2019-09-01
+@Copyright: Copyright (C) Huangying Zhan 2020. All rights reserved. Please refer to the license file.
+@LastEditTime: 2020-05-20
+@LastEditors: Huangying Zhan
+@Description: Timer object for counting times.
+'''
 
 import numpy as np
 from time import time
 
 
-class Timers():
+class Timer():
+    """Timer class to count time and do time analysis
+    """
+
     def __init__(self, items=None):
+        """
+        Args:
+            items (list/str): list of items to be counted, each item is a str
+        """
         self.timers = {}
         if items is not None:
             self.add(items)
 
-    def add(self, item):
+    def add(self, item, group=None):
         """add item to the timer
+        
         Args:
             item (str/list): item name
+            group (str): group name of the item
         """
         if isinstance(item, list):
             for i in item:
-                self.timers[i] = []
+                self.timers[i] = {
+                    'name': i,
+                    'time': 0,
+                    'is_counting': False,
+                    'duration': [],
+                    'group': group
+                }
         elif isinstance(item, str):
-            self.timers[item] = []
+            self.timers[item] = {
+                    'name': item,
+                    'time': 0,
+                    'is_counting': False,
+                    'duration': [],
+                    'group': group
+                }
         else:
             assert False, "only list or str is accepted."
     
-    def count(self, item, duration):
-        """add duration of an item to timer
+    def start(self, item, group=None):
+        """Start timer for an item
+
+        Args:
+            item (str): timer name
+            group (str): group name for the item
         """
         if self.timers.get(item, -1) == -1:
-            self.timers[item] = []
-        self.timers[item].append(duration)
+            self.add(item, group)
 
+        assert not(self.timers[item]['is_counting']),  "Timer for {} has started already.".format(item)
+        
+        self.timers[item]['is_counting'] = True
+        self.timers[item]['time'] = time()
+    
+    def end(self, item):
+        """Stop timer for an item
+
+        Args:
+            item (str): timer name
+        """
+        assert self.timers[item]['is_counting'], "Timer for {} has not started.".format(item)
+        
+        duration = time() - self.timers[item]['time']
+        self.timers[item]['duration'].append(duration)
+        self.timers[item]['is_counting'] = False
+    
+    def time_analysis(self):
+        """Time analysis of the items
+        """
+        print("----- time breakdown -----")
+        # group items according to groups
+        group_timers = {'single': []}
+        for key in sorted(self.timers.keys()):
+            group_name = self.timers[key]['group']
+            if group_name is not None:
+                if group_timers.get(group_name, -1) == -1:
+                    group_timers[group_name] = []
+                group_timers[group_name].append(self.timers[key])
+            else:
+                group_timers['single'].append(self.timers[key])
+        
+        # display times
+        for group_name, members in group_timers.items():
+            print("Group [{}]: ".format(group_name))
+            group_avg_times = []
+            for member in members:
+                avg_time = np.asarray(member['duration']).mean()
+                group_avg_times.append(avg_time)
+                print("\t[{}]: {:.03f}s".format(member['name'], avg_time))
+            if group_name != 'single':
+                print("Group [{}]'s total time: {:.03f}s".format(group_name, np.asarray(group_avg_times).sum()))

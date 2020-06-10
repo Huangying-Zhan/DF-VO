1f30eb6e5e6f53ce27387e6fa74c0bcd6a56182f
Huangying-Zhan <zhanhuangying@gmail.com>
2020-06-10 22:33:16 +0930

    (tmp) for debug

--- scripts/run_kitti.sh
+++ scripts/run_kitti.sh
@@ -1,8 +1,11 @@
 ############# Unit Testing #############
+# kinect
+# python apis/run.py -d tmp/kejie_kinect_new.yml --no_confirm
+
 # Reference 
-# python apis/run.py -d options/kitti/tro_exp/4_0.yml 
-# python apis/run.py -d options/kitti/tro_exp/4_1.yml 
-python apis/run.py -d options/unit_test/default.yml --no_confirm
+# python apis/run.py -d options/unit_test/default.yml --no_confirm
+python apis/run.py -d options/kitti/tro_exp/17_0.yml --no_confirm
+# python apis/run.py -d options/kitti/tro_exp/4_1.yml  
 # python apis/run.py -d options/unit_test/default.yml -c options/unit_test/kitti_0.yml  --no_confirm
 
 # python apis/run.py -d options/unit_test/default.yml -c options/unit_test/kitti_0.yml  --no_confirm
@@ -55,15 +58,19 @@ python apis/run.py -d options/unit_test/default.yml --no_confirm
 
 ############# Run odom seqs #############
 # for seq in \
-# 0 1 2 3 4 5 6 7 8 9 10
+# 9 10
+# # 0 1 2 3 4 5 6 7 8 9 10
 # # 4
-# # 9 10
 # # 9 10 4 7 0 1 2 3 5 6 8 
 # # # 2 3 5 6 7 8
 # do
+#     python apis/run.py -s $seq -d options/kitti/tro_exp/16_2.yml --no_confirm
+#     python apis/run.py -s $seq -d options/kitti/tro_exp/16_3.yml --no_confirm
+#     python apis/run.py -s $seq -d options/kitti/tro_exp/16_4.yml --no_confirm
+#     # python apis/run.py -s $seq -d options/kitti/tro_exp/13_2.yml --no_confirm
 #     # python apis/run.py -s $seq -d options/kitti/tro_exp/14_0.yml --no_confirm
 #     # python apis/run.py -s $seq -d options/kitti/tro_exp/13_1.yml --no_confirm
-#     python apis/run.py -s $seq -d options/kitti/tro_exp/13_2.yml --no_confirm
+#     # python apis/run.py -s $seq -d options/kitti/tro_exp/13_2.yml --no_confirm
 #     # python run.py -s $seq -d result/dfvo2/tro_paper/0/0/configuration_09.yml -c options/kitti/tro_exp/reference.yml  --no_confirm
 #     # python run.py -s $seq -c options/kitti/dfvo_test.yml --no_confirm
 # done
@@ -101,7 +108,10 @@ python apis/run.py -d options/unit_test/default.yml --no_confirm
 #  ./tools/evaluation/flow/kitti_flow_2012/evaluate_flow_train kitti2012/lfn_odom09/epoch9/
 
 ############# Evaluation Odometry #############
-# python tools/evaluation/odometry/eval_odom.py \
-# --result result/dfvo2/tro_paper/15/1 \
-# --align 6dof \
-# --gt dataset/kitti_raw_pose
+python tools/evaluation/odometry/eval_odom.py \
+--result result/dfvo2/tro_paper/17/0 \
+--align 6dof \
+--gt dataset/kitti_raw_pose \
+--seq 2011_10_03_drive_0047_sync
+
+# --result result/tmp/0 \
